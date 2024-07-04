38f865e9bd75acfcc6a8e7c486b7895674078359
Diksh Kaushik <148184913+Scridiksh@users.noreply.github.com>
2024-07-04 13:24:08 +0000

    Datatypes and variables

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,10 @@
+"use strict" ;
+
+let name ="hitesh" //string
+let age = 34 // number 
+let alpha = 383633838637637353753573573375375375375373573573573573573373734737373473743463464363463 // bigint
+let state; //undefined
+let isLoggedin = false // boolean
+//null
+//objects
+//symbol
---
+++ 01-basics/variables.js
@@ -0,0 +1,9 @@
+const accountID = 1445345;
+let accountEmail =  "hitesh@google.in"
+let accountState
+var account_password ="12345"
+accountCity = "bengaluru";
+
+accountEmail = "hitesh @gmail.com"
+
+console.table([accountID ,accountEmail,accountState,account_password,accountCity])
